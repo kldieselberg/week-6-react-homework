1fd47a9524cf6a0de3c911f049bdbf4fc1d8a5dc
kldieselberg <kldieselberg@gmail.com>
2024-04-21 17:23:28 -0500

    added bootstrap

--- src/App.js
+++ src/App.js
@@ -1,15 +1,19 @@
 import "./App.css";
+import Weather from "./weather";
 
 export default function App() {
   return (
     <div className="App">
-      Weather App
-      <footer>
-        This project is open sourced on{" "}
-        <a href="https://github.com/kldieselberg/week-6-react-homework">
-          Github
-        </a>{" "}
-      </footer>
+      <div className="container">
+        <h1>Weather App</h1>
+        <Weather />
+        <footer>
+          This project is open sourced on{" "}
+          <a href="https://github.com/kldieselberg/week-6-react-homework">
+            Github
+          </a>{" "}
+        </footer>
+      </div>
     </div>
   );
 }
---
+++ src/weather.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./Weather.css";
+
+export default function Weather() {
+  return (
+    <div className="Weather">
+      Hello from Weather
+      <h1>New York</h1>
+      <ul>
+        <li>Wednesday 0:700</li>
+        <li>Wednesday 0:700</li>
+      </ul>
+    </div>
+  );
+}
