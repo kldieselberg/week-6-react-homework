e6c0ce0986edba84f5bce4f1db0caf5addfcae67
kldieselberg <kldieselberg@gmail.com>
2024-04-29 08:49:02 -0500

    updated structureDate

--- src/structureDate.js
+++ src/structureDate.js
@@ -14,12 +14,12 @@ export default function StructureDate(props) {
   let day = days[props.date.getDay()];
   let hours = props.date.getHours();
   if (hours < 10) {
-    hours = "0${hours}";
+    hours = `0${hours}`;
   }
 
   let minutes = props.date.getMinutes();
   if (minutes < 10) {
-    minutes = "${minutes}";
+    minutes = `${minutes}`;
   }
 
   return (
